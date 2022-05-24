13cc2df484c5f8ea8604dd2567814c2d2dd84f55
Donovan Griego <dxgrieg@sandia.gov>
2022-05-24 22:04:57 +0000

    mostly done with mnist example

--- DCNN.py
+++ DCNN.py
@@ -1,13 +1,166 @@
-# Base Layer Class
+from zoneinfo import available_timezones
+import numpy as np
+from keras.datasets import mnist
+from keras.utils import np_utils
+
+def tanh(x):
+    return np.tanh(x)
+
+def tanh_prime(x):
+    return 1 - np.tanh(x)**2
+
+# Base Layer
 class Layer:
     def __init__(self):
         self.input = None
         self.output = None
 
     # Computes the output Y of a layer for a given input X
-    def forward_propogation(self, input):
+    def forward_propagation(self, input_data):
         raise NotImplementedError
 
     # Computes dE/dX for a given dE/dY (and update paramters)
-    def backward_propogation(self, output_error, learning_rate):
+    def backward_propagation(self, output_error, learning_rate):
         raise NotImplementedError
+
+# Fully Connected Layer
+class FCLayer(Layer):
+    def __init__(self, input_size, output_size):
+        self.weights = np.random.rand(input_size, output_size)
+        self.bias = np.random.rand(1, output_size) - 0.5
+
+    def forward_propagation(self, input_data):
+        self.input = input_data
+        self.output = np.dot(self.input, self.weights) + self.bias
+        return self.output
+
+    def backward_propagation(self, output_error, learning_rate):
+        input_error = np.dot(output_error, self.weights.T)
+        weights_error = np.dot(self.input.T, output_error)
+        # dBias = output_error
+
+        # update parameters
+        self.weights -= learning_rate * weights_error
+        self.bias -= learning_rate * output_error
+        return input_error
+
+class ActivationLayer(Layer):
+    def __init__(self, activation, activation_prime):
+        self.activation = activation
+        self.activation_prime = activation_prime
+
+    # Returns the activated input
+    def forward_propagation(self, input_data):
+        self.input = input_data
+        self.output = self.activation(self.input)
+        return self.output
+
+    # Returns input_error=dE/dX for a given output_error=dE/dY
+    # learning_rate is not used beacuse there is no "learnable" parameters
+    def backward_propagation(self, output_error, learning_rate):
+        return self.activation_prime(self.input) * output_error
+
+def mse(y_true, y_pred):
+    return np.mean(np.power(y_true-y_pred, 2))
+
+def mse_prime(y_true, y_pred):
+    return 2*(y_pred-y_true)/y_true.size
+
+class Network:
+    def __init__(self):
+        self.layers = []
+        self.loss = None
+        self.loss_prime = None
+
+    # Add layer to network
+    def add(self, layer):
+        self.layers.append(layer)
+
+    # Set loss to use
+    def use(self, loss, loss_prime):
+        self.loss = loss
+        self.loss_prime = loss_prime
+
+    # Predict output for given input
+    def predict(self, input_data):
+        # Sample dimension first
+        samples = len(input_data)
+        result = []
+
+        # Run network over all samples
+        for i in range(samples):
+            # Forward propagation
+            output = input_data[i]
+            for layer in self.layers:
+                output = layer.forward_propagation(output)
+            result.append(output)
+        
+        return result
+
+    # Train the network
+    def fit(self, x_train, y_train, epochs, learning_rate):
+        # Sample dimension first
+        samples = len(x_train)
+
+        # Training loop
+        for i in range(epochs):
+            err = 0
+            for j in range(samples):
+                # Forward propagation
+                output = x_train[j]
+                for layer in self.layers:
+                    output = layer.forward_propagation(output)
+
+                # Compute loss (for display purposes)
+                err += self.loss(y_train[j], output)
+
+                # Backward propagation
+                error = self.loss_prime(y_train[j], output)
+                for layer in reversed(self.layers):
+                    error = layer.backward_propagation(error, learning_rate)
+
+            # Calculate average error on all samples
+            err /= samples
+            print(f"epoch {i + 1}/{epochs}  error={err}")
+
+
+
+# load MNIST from server
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# training data : 60000 samples
+# reshape and normalize input data
+x_train = x_train.reshape(x_train.shape[0], 1, 28*28)
+x_train = x_train.astype('float32')
+x_train /= 255
+# encode output which is a number in range [0,9] into a vector of size 10
+# e.g. number 3 will become [0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+y_train = np_utils.to_categorical(y_train)
+
+# same for test data : 10000 samples
+x_test = x_test.reshape(x_test.shape[0], 1, 28*28)
+x_test = x_test.astype('float32')
+x_test /= 255
+y_test = np_utils.to_categorical(y_test)
+
+# Network
+net = Network()
+net.add(FCLayer(28*28, 100))                # input_shape=(1, 28*28)    ;   output_shape=(1, 100)
+net.add(ActivationLayer(tanh, tanh_prime))
+net.add(FCLayer(100, 50))                   # input_shape=(1, 100)      ;   output_shape=(1, 50)
+net.add(ActivationLayer(tanh, tanh_prime))
+net.add(FCLayer(50, 10))                    # input_shape=(1, 50)       ;   output_shape=(1, 10)
+net.add(ActivationLayer(tanh, tanh_prime))
+
+# train on 1000 samples
+# as we didn't implemented mini-batch GD, training will be pretty slow if we update at each iteration on 60000 samples...
+net.use(mse, mse_prime)
+net.fit(x_train[0:1000], y_train[0:1000], epochs=100, learning_rate=0.1)
+
+# test on 3 samples
+out = net.predict(x_test[0:3])
+print("\n")
+print("predicted values : ")
+print(out, end="\n")
+print("true values : ")
+print(y_test[0:3])
